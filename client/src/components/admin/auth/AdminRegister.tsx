c3c0f43e1eee12727b99d7ab5d6a527a96163708
Alex Orpi <orpi.alex@hotmail.com>
2023-11-24 19:27:55 +0100

    login and register

---
+++ client/src/components/admin/auth/AdminRegister.tsx
@@ -0,0 +1,68 @@
+// import React from 'react';
+
+import { Button } from '../../utils/Button';
+
+// type Props = {};
+
+const AdminRegister = () => {
+  return (
+    <>
+      <h1 className="text-gray-700 text-4xl font-medium mt-10 border-b border-gray-400 pb-2">
+        Registrar nou membre
+      </h1>
+      <div className="bg-slate-300 rounded-xl mt-6 p-6 space-y-4 md:space-y-6 sm:p-8">
+        <form className="" action="#">
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-1 text-sm font-semibold text-gray-900"
+            >
+              El teu email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5 mb-3"
+              placeholder="email@email.com"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block mb-1 text-sm font-semibold text-gray-900"
+            >
+              Contrasenya
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••"
+              className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5 "
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="confirmPassword"
+              className="block mb-1 text-sm font-semibold text-gray-900"
+            >
+              Contrasenya
+            </label>
+            <input
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              placeholder="••••••••"
+              className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5 "
+            />
+          </div>
+          <p className="text-sm font-light my-3">Ja estàs registrat? Accedeix</p>
+          <Button title="Accedir" to="/admin/login" />
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default AdminRegister;
--- client/src/components/utils/Button.tsx
+++ client/src/components/utils/Button.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-type Props = { title: string };
+type Props = { title: string; to: string };
 
 export const Button = (props: Props) => {
   return (
     <a
-      href="/admin"
+      href={props.to}
       className="btn text-sky-950 border-sky-950 md:border-2 hover:bg-sky-950 hover:text-white transition ease-out duration-500"
     >
       {props.title}
