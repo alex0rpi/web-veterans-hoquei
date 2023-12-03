fd5967e1e7a8855ef01780e6fa7e321a63a50254
Alex Orpi <orpi.alex@hotmail.com>
2023-12-03 10:43:29 +0100

    increased slider height and added circle dots

--- client/src/components/main/ImageSlider.tsx
+++ client/src/components/main/ImageSlider.tsx
@@ -4,6 +4,7 @@ import {
   faChevronLeft,
   faChevronRight,
   faCircle,
+  faCircleDot,
 } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
@@ -31,8 +32,8 @@ const ImageSlider = () => {
     }
 
   return (
-    <div className="relative m-auto h-[340px] w-full px-4 pt-4 group">
-        <div style={{backgroundImage: `url(${slides[currentIndex]})`}} className="w-full h-full rounded-2xl bg-center bg-cover duration-200"></div>
+    <div className="relative m-auto h-[600px] w-full px-4 pt-4 group">
+        <div style={{backgroundImage: `url(${slides[currentIndex]})`, backgroundSize: 'cover'}} className="w-full h-full rounded-2xl duration-200"></div>
         <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-200 absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 cursor-pointer text-white w-10 h-10 flex items-center justify-center">
         <FontAwesomeIcon
         icon={faChevronLeft}
@@ -52,7 +53,16 @@ const ImageSlider = () => {
         <div className="flex top-4 justify-center py-2">
            { slides.map((slide, slideIndex) => (
             <div key={slideIndex} className="cursor-pointer">
+              {slideIndex === currentIndex 
+              ? (
+
+                <FontAwesomeIcon icon={faCircleDot} size="2xs" style={{color: "rgb(8 47 73)"}} className="hover:scale-125 mx-2" onClick={() => goToSlideHandler(slideIndex)}/>
+              ) 
+              : (
+
                 <FontAwesomeIcon icon={faCircle} size="2xs" style={{color: "rgb(8 47 73)"}} className="hover:scale-125 mx-2" onClick={() => goToSlideHandler(slideIndex)}/>
+              )
+              }
             </div>
            ))
         }
