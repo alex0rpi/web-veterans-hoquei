c28b7303a14c2e85b768134bfb78f1520f2582f0
Alex Orpi <orpi.alex@hotmail.com>
2023-12-11 19:10:53 +0100

    Added .env template and removed some files, corrected .env var name

--- client/src/services/PdfViewer.tsx
+++
@@ -1,9 +0,0 @@
-const PDFViewer = () => {
-  return (
-    <div>
-      <iframe src="../assets/Documents/07-Llibre-optimitzat.pdf" width={80} height={80} />
-    </div>
-  );
-};
-
-export default PDFViewer;
