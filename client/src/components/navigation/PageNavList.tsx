8f64d16fe6312a8955863b87239a1eaf15f0ef35
AlexOrpi <orpi.alex@hotmail.com>
2024-04-07 20:31:12 +0200

    Page and season transitions

--- client/src/components/navigation/PageNavList.tsx
+++ client/src/components/navigation/PageNavList.tsx
@@ -30,7 +30,7 @@ const PageNavList = ({ homeRef }: TPageNav) => {
   const isBook = location.pathname === paths.book;
   // const isPlayers = location.pathname === paths.players;
   // const isBlog = location.pathname === paths.blog;
-  // const isSeason = location.pathname.startsWith(paths.season.split(':')[0]);
+  const isSeason = location.pathname.startsWith(paths.season.split(':')[0]);
   const adminPaths = [
     paths.me,
     paths.newChapter,
@@ -79,25 +79,12 @@ const PageNavList = ({ homeRef }: TPageNav) => {
         to={paths.book}
         highlight={isBook}
       />
-      {location.pathname === paths.book ? (
-        <NavItem
-          icon={faCalendar}
-          title={
-            <>
-              <span>Temporades</span>
-              <br />
-              <span>recents</span>
-            </>
-          }
-          to={paths.genericSeason}
-        />
-      ) : (
-        <NavItem
-          icon={faCalendar}
-          title='Temporades recents'
-          to={paths.genericSeason}
-        />
-      )}
+      <NavItem
+        icon={faCalendar}
+        title='Temporades recents'
+        to={paths.genericSeason}
+        highlight={isSeason}
+      />
 
       {/*       <NavItem
         icon={faPersonSkating}
